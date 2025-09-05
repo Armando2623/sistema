dd4ad527646dc59e096ae38b112e9ba3b6eac991
Juan2623 <armando.ortizvegas@gmail.com>
2025-09-04 22:28:57 -0500

    Actualizacion del JS

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ let amigos = [];
 
 
 
-function agregaAmigos(){
-    let valor = document.getElementById("amigo")
+function agregaAmigos() {
+    let valor = document.getElementById("amigo");
 
-    if(valor.value.trim() === ""){
-        alert("Escribir un valor correcto")
+    if (valor.value.trim() === "") {
+        alert("Escribir un valor correcto");
     }
-        else
-            amigos.push(valor);
-            console.log(amigos.value)
-     valor.value = "";
-        valor.focus();
+    else
+    amigos.push(valor);
+    console.log(amigos.value)
+    valor.value = "";
+    valor.focus();
+
+}
+function verAmigos(){
+    let list = document.getElementById("listaAmigos");
+    list.innerHTML = "";
+    let valor = document.getElementById("amigo");
 }
