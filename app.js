e5699bf8fd08fad4582bc81d3ca7721cde00acbd
Armando2623 <armando.ortizvegas@gmail.com>
2025-09-16 11:07:14 -0500

    Actualización

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
 let amigos = [];
- let valor = document.getElementById("amigo");
+let valor = document.getElementById("amigo");
 
 
 
 
 function agregaAmigos() {
-   
+
     if (valor.value.trim() === "") {
         alert("Escribir un valor correcto");
     }
     else
-    amigos.push(valor.value);
-   console.log(valor.value)
-   let li = document.createElement("li");
-        li.textContent = valor.value;
-        document.getElementById("listaAmigos").appendChild(li);
+     amigos.push(valor.value);
+    mostrarEnPantalla();
     valor.value = "";
     valor.focus();
 
 }
-function verAmigos(){
-   
-    
-    
-}
-function sortearAmigo(){
-    if (amigos.length === 0){
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
         console.log("Agrega nombres de amigos")
     }
     let indice = Math.floor(Math.random() * amigos.length);
-   
-  console.log(amigos[indice]);
+
+    console.log(amigos[indice]);
 
 }
 
-function asignarTextoElemento(){
+function asignarTextoElemento() {
 
+}
+function mostrarEnPantalla(){
+    let li = document.createElement("li");
+    li.textContent = valor.value;
+    document.getElementById("listaAmigos").appendChild(li);
 }
